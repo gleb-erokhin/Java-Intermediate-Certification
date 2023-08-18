c27dc7f27142bcb273dad9c5faa115672bd6bce0
gleb.erokhin <gleb.erokhin@gmail.com>
2023-08-18 13:55:22 +0300

    создание класса Toy, get+set

---
+++ src/Toy.java
@@ -0,0 +1,38 @@
+public class Toy {
+    private int id;
+    private String name;
+    private double frequency;
+
+
+    public Toy(int id, String name, double frequency) {
+        this.id = id;
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getFrequency() {
+        return frequency;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFrequency(double frequency) {
+        this.frequency = frequency;
+    }
+}
